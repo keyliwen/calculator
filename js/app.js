600e6c134356b7ced8dbb119c088e62def8928e0
keyliwen <390616209@qq.com>
2015-11-01 10:24:40 +0800

    增加运算

--- js/app.js
+++ js/app.js
@@ -15,9 +15,11 @@ var sNumber = "";
 var sOperator = "";
 //是否要清除输入框中已有的内容
 var clearScreen = false;
+//是否第一次输入
+var isFirstInput = true;
 
-//计算
 function calculate(number1, number2, sOperator){
+  console.log(number1 + "," + number2);
   var result = 0;
   switch(sOperator) {
     case "+":
@@ -37,17 +39,62 @@ function calculate(number1, number2, sOperator){
   }
   return result;
 }
+//字符串转数字
+function parseToNumber(str) {
+  if(str.indexOf(".") > 0){
+    return parseFloat(str);
+  }else if(str.indexOf(".") == 0){
+    return parseFloat(0 + str);
+  }else{
+    return parseInt(str); 
+  }
+}
 
 //输入
 function doInput(){
-  var input = this.data();
+  var input = $(this).data("input");
+  var screenVal = screenCal.text();
   switch(input){
     case "=":
-      screenCal.text(parseInt(sNumber), parseInt(input), sOperator);
+      screenCal.text(calculate(parseToNumber(sNumber), parseToNumber(screenVal), sOperator));
       sNumber = "";
       sOperator = "";
       clearScreen = true;
+      isFirstInput = true;
       break;
+    case "+":
     case "-":
+    case "÷":
+    case "×":
+      clearScreen = true;
+
+      if(sNumber.length != 0){
+        screenCal.text(calculate(parseToNumber(sNumber),parseToNumber(screenVal),sOperator));
+      }
+      sOperator = input;
+      sNumber = screenVal;
+      break;
+    case "c":
+      screenCal.text("0");
+      sNumber = "";
+      sOperator = "";
+      break;
+    default:   //数字
+      if(clearScreen){
+        sNumber = screenVal;
+        screenCal.empty();
+        clearScreen = false;
+      }
+      if (isFirstInput) {
+        screenCal.empty().append(input);
+        isFirstInput = false;  
+      }else {
+        screenCal.append(input);
+      }
+      break;
   }
 }
+
+$(document).ready(function(){
+  $(".clear,.number-key,.operator,.eval").on("click",doInput);
+});
